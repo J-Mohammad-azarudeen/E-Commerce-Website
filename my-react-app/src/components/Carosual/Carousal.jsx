f501b366482e7d72be098d5c20153941b8ecb104
Sheik636 <sheikmusthak006@gmail.com>
2025-05-10 12:15:49 +0530

    creating a slides

--- my-react-app/src/components/Carosual/Carousal.jsx
+++ my-react-app/src/components/Carosual/Carousal.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import image from 'src/assets/photorealistic-wild-tuna-day-celebration_23-2151307884.avif';
 
 export default function Carousal() {
   return (
     <div>
-      <h1>Hello </h1>
+      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={image} className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..."/>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   )
 }

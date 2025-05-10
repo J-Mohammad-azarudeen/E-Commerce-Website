afa3ddff1b08dcb230cbbd5c66305327cbc4239e
DarkAzarWorld <azarrock1000@gmail.com>
2025-05-10 11:08:48 +0530

    Created a corousal component and css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Carousal from './components/Carosual/Carousal'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-    
+    <Navbar />
+    <Carousal />
    </div>
   )
 }

822c8a8eef5672b195f161f50a828b00ed0a1e9d
RajiNila <110461660+RajiNila@users.noreply.github.com>
2025-05-13 16:06:25 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,18 +6,12 @@ import Carousal from './components/Carosual/Carousal'
 import Navbar from './components/Navbar/Navbar'
 import ProductCard from './components/Productcard/Productcard.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const sampleProduct = {
-name: 'Wireless Headphones',
-price: 59.99,
-imageUrl: 'https://via.placeholder.com/300x200?text=Product+Image',
-};
+
 
   return (
   <div>
     <Navbar />
-    <Carousal />
+    
    
    
   </div>

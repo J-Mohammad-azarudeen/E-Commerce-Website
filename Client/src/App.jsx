b2bf6c865dc27e5ec85756fbd6e8adf81ac9fa9d
DarkAzarWorld <azarrock1000@gmail.com>
2025-05-13 13:04:50 +0530

    Created a product card components

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,15 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Carousal from './components/Carosual/Carousal'
 import Navbar from './components/Navbar/Navbar'
+import ProductCard from './components/Productcard/Productcard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const sampleProduct = {
+name: 'Wireless Headphones',
+price: 59.99,
+imageUrl: 'https://via.placeholder.com/300x200?text=Product+Image',
+};
 
   return (
-   <div>
+  <div>
     <Navbar />
     <Carousal />
-   </div>
+   
+   
+  </div>
   )
 }
 

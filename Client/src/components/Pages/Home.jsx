9b91297a112a63ec2cbbb95f37d7a92b34f54b20
RajiNila <rajisky.t@gmail.com>
2025-05-13 16:36:57 +0530

    Header Navbar, Pages Folder created and react routre implemented.

---
+++ Client/src/components/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousal from '../Carosual/Carousal'
+
+const Home = () => {
+    return (
+        <div>
+            <Carousal />
+        </div>
+    )
+}
+
+export default Home

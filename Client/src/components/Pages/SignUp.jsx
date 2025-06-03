36999213cf54f4b808b4dea7df07906c8855e3c9
Sheik636 <sheikmusthak006@gmail.com>
2025-06-03 16:08:22 +0530

    I am Finished The Signup Page

--- Client/src/components/Pages/SignUp.jsx
+++ Client/src/components/Pages/SignUp.jsx
@@ -2,7 +2,89 @@ import React from 'react'
 
 const SignUp = () => {
     return (
-        <div>SignUp</div>
+        <div className="container mt-5">
+            <div class="row g-3">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="First name" aria-label="First name" />
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name" />
+                </div>
+            </div>
+            <form class="row g-3">
+  <div class="col-md-6">
+    <br />
+    {/* <label for="inputEmail4" class="form-label">Email</label> */}
+    <input type="email" class="form-control" id="inputEmail4" placeholder='Your Email' />
+  </div>
+  <div class="col-md-6">
+    <br />
+    {/* <label for="inputPassword4" class="form-label">Password</label> */}
+    <input type="password" class="form-control" id="inputPassword4" placeholder='Your Password' />
+  </div>
+  <div class="col-12">
+    
+    {/* <label for="inputAddress" class="form-label">Address</label> */}
+    <input type="text" class="form-control" id="inputAddress" placeholder="Address 1" />
+  </div>
+  <div class="col-12">
+    {/* <label for="inputAddress2" class="form-label">Address 2</label> */}
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Address 2" />
+  </div>
+  <div class="col-md-6">
+    {/* <label for="inputCity" class="form-label">City</label> */}
+    <input type="text" class="form-control" id="inputCity" placeholder='City' />
+  </div>
+  <div class="col-md-4">
+    {/* <label for="inputState" class="form-label">State</label> */}
+    <select id="inputState" class="form-select" placeholder='State'>
+      <option>State</option>
+      <option>Andhra Pradesh</option>
+      <option>Arunachal Pradesh</option>
+      <option>Assam</option>
+      <option>Bihar</option>
+      <option>Chhattisgarh</option>
+      <option>Goa</option>
+      <option>Gujarat</option>
+      <option>Haryana</option>
+      <option>Himachal Pradesh</option>
+      <option>Jharkhand</option>
+      <option>Karnataka</option>
+      <option>Kerala</option>
+      <option>Madhya Pradesh</option>
+      <option>Manipur</option>
+      <option>Meghalaya</option>
+      <option>Mizoram</option>
+      <option>Nagaland</option>
+      <option>Odisha</option>
+      <option>Punjab</option>
+      <option>Rajasthan</option>
+      <option>Sikkim</option>
+      <option>Tamil Nadu</option>
+      <option>Telangana</option>
+      <option>Tripura</option>
+      <option>Uttarakhand</option>
+      <option>Uttar Pradesh</option>
+      <option>West Bengal</option>
+    </select>
+  </div>
+  <div class="col-md-2">
+    {/* <label for="inputZip" class="form-label">Zip</label> */}
+    <input type="text" class="form-control" id="inputZip" placeholder='Pincode' />
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck" />
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <div class="col-12 text-center">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
+        </div>
     )
 }
 

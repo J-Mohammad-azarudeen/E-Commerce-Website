0e516201790a08ef5ba196217770689a2eeb18cd
Sheik636 <sheikmusthak006@gmail.com>
2025-06-12 22:46:37 +0530

    I am Changed Some Width and Height in Navbar

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                     </ul>
                     <form className="d-flex" role="search">
                         <div className="input-group">
-                            <input type="search" className="form-control w-[300px] " placeholder="Search" aria-label="Search" style={{ width: "500px" }} />
+                            <input type="search" className="form-control w-[300px] " placeholder="Search" aria-label="Search" style={{ width: "350px" }} />
                             <button className="btn btn-outline-primary h-90" type="submit">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="18" fill="currentColor"
                                     className="bi bi-search" viewBox="0 0 16 16">
@@ -74,6 +74,9 @@ const Navbar = () => {
                                         d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                                 </svg> Cart</Link>
                         </li>
+                        </ul>
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-4">
+
                         <li className="nav-item dropdown">
                             <button
                                 className="btn btn-outline-secondary nav-link fs-4 font-monospace fw-semibold text-uppercase dropdown-toggle rounded-2"
@@ -105,7 +108,6 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-
         </nav>
     );
 };

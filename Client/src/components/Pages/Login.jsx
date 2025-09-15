f2baa9cadd991647e299d2c8a16a4804a81dd76d
DarkAzarWorld <azarrock1000@gmail.com>
2025-09-15 11:14:41 +0530

    Created a state for loading and onsubmit in login page

--- Client/src/components/Pages/Login.jsx
+++ Client/src/components/Pages/Login.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setLoading(true);
+        try {
+            setLoading(false);
+            alert("Logged in Successfully");
+            
+        } catch (error) {
+            console.log(error);
+            alert("Something went wrong");
+        }
+        
+    }
     return (
         
         <div className="text-center container mt-5 mb-5" style={{ maxWidth: '500px', margin: 'auto' }}>
@@ -9,19 +25,19 @@ const Login = () => {
                 <h1>Welcome To Fishkart!</h1>
             </div>
             <br />
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
             
             <div class="col-md-12">
             <br />
-                <input type="email" class="form-control" id="inputEmail4" placeholder='Your Email' required />
+                <input type="email" name='email' class="form-control" id="inputEmail4" placeholder='Your Email' required />
             </div>
             <div class="col-md-12">
                 <br />
-                <input type="password" class="form-control" id="inputPassword4" placeholder='Your Password' required />
+                <input type="password" name='password' class="form-control" id="inputPassword4" placeholder='Your Password' required />
             </div>
             <div class="col-12 text-center">
                 <br />
-                <button type="submit" class="btn btn-primary">Log in</button>
+                <button type="submit" class="btn btn-primary">{loading ? "logging" : "Log in"}</button>
                 
                 <p>You Don't have a Account? Click <Link to="/SignUp">Sign in..!</Link></p>
             </div>
